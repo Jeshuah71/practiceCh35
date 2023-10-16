0c95b96008b95904a8956d445c6669a2c5e37fbd
Jeshua Herrera Bedoya <Jeshuah71>
2023-10-15 22:38:53 -0600

    PracticeFinished

--- Program.cs
+++ Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Runtime.Serialization;
+
+int GetNumberFromUser()
+{
+    int number = 0;
+    while (number < 1 || number > 10)
+    {
+        Console.WriteLine("Enter a number between 1 and 10: ");
+        string? response = Console.ReadLine();
+        try
+        {
+            number = Convert.ToInt32(response);
+
+        }
+        catch (FormatException e)
+        {
+            number = -1;
+            Console.WriteLine($"I do not understand '{response}'");
+            Console.WriteLine(e.Message);
+        }
+        catch (OverflowException)
+        {
+            number = -1;
+            Console.WriteLine($"That number is too big");
+        }
+    }
+    return number;
+}
+Console.WriteLine("Name an animal: ");
+string? animal = Console.ReadLine();
+if (animal == "Snake")
+{
+    throw new NotSupportedException("I have ophidiophobia");
+}
+public class Snakexception : Exception
+{
+    public Snakexception()
+    {
+
+    }
+
+    public Snakexception(string message) : base(message)
+    {
+        try
+        {
+            Console.WriteLine("Shall we play a game?");
+            if (Console.ReadLine() == "no") return;
+            Console.WriteLine("Name an animal.");
+            string? animal = Console.ReadLine();
+            if (animal == "snake") throw new Snakexception();
+        }
+        catch (Snakexception) { Console.WriteLine("Why did it have to be snakes?"); }
+        finally
+        {
+            Console.WriteLine("We are all done here");
+
+        }
+        
+    }
+}
